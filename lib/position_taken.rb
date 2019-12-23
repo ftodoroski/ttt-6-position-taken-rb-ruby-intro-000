bbbe0159121f363c6448188cfc5911cdcb691bf8
Filip Todoroski <ftodoroski@ymail.com>
2019-12-23 17:40:14 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,11 +6,11 @@
 #     return true
 #   end
 # end
-
-def position_taken?(board, index)
-  if board[index] == " " || "" || nil
-    return false
-  elsif board[index] == "X" || "O"
-    return true
-  end
-end
+# 
+# def position_taken?(board, index)
+#   if board[index] == " " || "" || nil
+#     return false
+#   elsif board[index] == "X" || "O"
+#     return true
+#   end
+# end
